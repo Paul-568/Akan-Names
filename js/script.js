75c63c82323787244063056734f675c70ebdea7e
Paul <ptuleto@gmail.com>
2020-05-10 22:01:48 +0300

    add a day validator

--- js/script.js
+++ js/script.js
@@ -10,3 +10,26 @@ function getAkanName () {
       }
     }
   }
+  let myGenderValue = getGender();
+  console.log(myGenderValue);
+  unction dayValidator () {
+    if (monthOfBirth === 2 && Number(yearOfBirth)%4 === 0) {
+      if (dayOfBirth > 28 || dayOfBirth < 1) {
+        return false;
+      } else if (monthOfBirth === 2 && dayOfBirth > 29) {
+        return false;
+      } else if (monthOfBirth === 2 && dayOfBirth < 1) {
+        return false;
+      } else {
+        return true;
+      }
+    } else if (dayOfBirth < 1 || dayOfBirth > 31){
+      return false;
+    } else {
+      return true;
+    }
+  }
+  let monthValid = monthValidator();
+  let dayValid = dayValidator();
+  let dayOfWeekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+
+          ((5*Number(yearOfBirth.slice(2,4))/4))+((26*(monthOfBirth+1)/10))+dayOfBirth)%7);
